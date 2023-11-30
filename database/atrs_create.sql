97bc94c32abad8888fe7de390d62d1a0ab19edf4
Xinyu Li <eigerli@outlook.com>
2023-11-30 16:17:58 +0800

    fix database table creation, add more rows

--- database/atrs_create.sql
+++ database/atrs_create.sql
@@ -3,96 +3,96 @@ Convention of datatype
 - name: VARCHAR(100)
 - id: VARCHAR(10)
 - password: VARCHAR(100)
-- num: INT(20)
 - email VARCHAR(100)
 
 */
 
-#USE atrs;
+USE atrs;
 
 CREATE TABLE airline(
-	name	VARCHAR(100),
-	PRIMARY KEY(name)
+    name VARCHAR(100) UNIQUE NOT NULL,
+    PRIMARY KEY(name)
 );
 
 CREATE TABLE airline_staff(
-	username		VARCHAR(100),
-	password		VARCHAR(100) NOT NULL,
-	first_name		VARCHAR(100),
-	last_name		VARCHAR(100),
-	date_of_birth	TIMESTAMP,
-	permission		VARCHAR(10),
-	airline_name	VARCHAR(100),
-	PRIMARY KEY(username),
-	FOREIGN KEY(airline_name) REFERENCES airline(name)
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    date_of_birth DATE,
+    permission VARCHAR(10),
+    airline_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(username),
+    FOREIGN KEY(airline_name) REFERENCES airline(name)
 );
 
 CREATE TABLE airplane(
-	id				VARCHAR(10),
-	airline_name 	VARCHAR(100),	
-	PRIMARY KEY(id, airline_name),
-	FOREIGN KEY(airline_name) REFERENCES airline(name)
+    airplane_id INT AUTO_INCREMENT,
+    airline_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(airplane_id),
+    FOREIGN KEY(airline_name) REFERENCES airline(name)
 );
 
 CREATE TABLE airport(
-	name	VARCHAR(100) NOT NULL,
-	city	VARCHAR(100),
-	PRIMARY KEY(name)
+    name VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    PRIMARY KEY(name)
 );
 
 CREATE TABLE booking_agent(
-	email				VARCHAR(100),
-	password			VARCHAR(100) NOT NULL,
-	booking_agent_id	VARCHAR(10) NOT NULL,
-	airline_name		VARCHAR(100),
-	PRIMARY KEY(email),
-	FOREIGN KEY(airline_name) REFERENCES airline(name)
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    booking_agent_id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    airline_name VARCHAR(100),
+    PRIMARY KEY(email),
+    FOREIGN KEY(airline_name) REFERENCES airline(name)
 );
 
 CREATE TABLE flight(
-	flight_num			INT(20),
-	airline_name		VARCHAR(100),
-	departure_time		DATETIME,
-	arrival_time		DATETIME,
-	price				NUMERIC(15, 5),		
-	status				VARCHAR(10),
-	airplane_id			VARCHAR(10),
-	arr_airport_name	VARCHAR(100),
-	dept_airport_name	VARCHAR(100),
-	PRIMARY KEY(flight_num, airline_name),
-	FOREIGN KEY(airline_name) REFERENCES airline(name),
-	FOREIGN KEY(arr_airport_name) REFERENCES airport(name),
-	FOREIGN KEY(dept_airport_name) REFERENCES airport(name)
+    flight_num INT AUTO_INCREMENT NOT NULL,
+    airline_name VARCHAR(100) NOT NULL,
+    departure_time DATETIME NOT NULL,
+    arrival_time DATETIME NOT NULL,
+    price NUMERIC(15, 5) NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    airplane_id INT NOT NULL,
+    arr_airport_name VARCHAR(100) NOT NULL,
+    dept_airport_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(flight_num, airline_name),
+    FOREIGN KEY(airplane_id) REFERENCES airplane(airplane_id),
+    FOREIGN KEY(airline_name) REFERENCES airline(name),
+    /* FOREIGN KEY(airplane_id, airline_name) REFERENCES airplane(airplane_id, airline_name), */
+    FOREIGN KEY(arr_airport_name) REFERENCES airport(name),
+    FOREIGN KEY(dept_airport_name) REFERENCES airport(name)
 );
 
 CREATE TABLE customer(
-	email				VARCHAR(100),
-	name				VARCHAR(100),
-	password			VARCHAR(100),
-	building_number		VARCHAR(100),
-	street				VARCHAR(100),
-	city				VARCHAR(100),
-	state				VARCHAR(100),
-	phone_number		VARCHAR(20),
-	passport_number		VARCHAR(100),
-	passport_expiration	VARCHAR(100),
-	passport_country	VARCHAR(100),
-	date_of_birth		TIMESTAMP,
-	PRIMARY KEY(email)
+    email VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    building_number VARCHAR(100),
+    street VARCHAR(100),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    phone_number VARCHAR(20) NOT NULL,
+    passport_number VARCHAR(100) NOT NULL UNIQUE,
+    passport_expiration DATE NOT NULL,
+    passport_country VARCHAR(100) NOT NULL,
+    date_of_birth TIMESTAMP NOT NULL,
+    PRIMARY KEY(email)
 );
 
 CREATE TABLE ticket(
-	ticket_id				VARCHAR(10),
-	flight_num				INT(20),
-	airline_name			VARCHAR(100),
-	customer_email			VARCHAR(100),
-	booking_agent_email		VARCHAR(100),
-	PRIMARY KEY(ticket_id),
-	FOREIGN KEY(flight_num) REFERENCES flight(flight_num),
-	FOREIGN KEY(airline_name) REFERENCES airline(name),
-	FOREIGN KEY(customer_email) REFERENCES customer(email),
-	FOREIGN KEY(booking_agent_email) REFERENCES booking_agent(email)
+    ticket_id INT AUTO_INCREMENT,
+    flight_num INT NOT NULL,
+    airline_name VARCHAR(100) NOT NULL,
+    customer_email VARCHAR(100) NOT NULL,
+    booking_agent_email VARCHAR(100),
+    PRIMARY KEY(ticket_id),
+    FOREIGN KEY(flight_num) REFERENCES flight(flight_num),
+    FOREIGN KEY(airline_name) REFERENCES flight(airline_name),
+    /* FOREIGN KEY(flight_num, airline_name) REFERENCES flight(flight_num, airline_name), */
+
+    FOREIGN KEY(customer_email) REFERENCES customer(email),
+    FOREIGN KEY(booking_agent_email) REFERENCES booking_agent(email)
 );
-
-
-
--- database/atrs_insert.sql
+++ database/atrs_insert.sql
@@ -1,37 +1,61 @@
 USE atrs;
 
--- a
-INSERT INTO airline (name) VALUES ('China Eastern');
+-- Adding airlines for diversity in flight options
+INSERT INTO airline (name) 
+VALUES ('China Eastern'),
+       ('American Airlines'),
+       ('Delta Air Lines');
 
--- b
+-- Adding airports to cover different cities
 INSERT INTO airport (name, city) 
-VALUES ('JFK', 'NYC'),
-	('PVG', 'Shanghai');
+VALUES ('JFK', 'New York'),
+       ('PVG', 'Shanghai'),
+       ('LAX', 'Los Angeles'),
+       ('HND', 'Tokyo'),
+       ('LHR', 'London');
 
--- c
+-- Adding customers with diverse destinations and origins
 INSERT INTO customer (email, name, password, building_number, street, city, state, phone_number, passport_number, passport_expiration, passport_country, date_of_birth) 
-VALUES ('customer1@example.com', 'Customer One', 'password1', '123', 'Street 1', 'City 1', 'State 1', '1234567890', 'P12345678', '2025-12-31', 'Country 1', '1990-01-01'),
-       ('customer2@example.com', 'Customer Two', 'password2', '456', 'Street 2', 'City 2', 'State 2', '0987654321', 'P23456789', '2026-12-31', 'Country 2', '1991-01-01');
+VALUES ('customer1@example.com', 'Oliver One', 'password1', '123', 'Street 1', 'City 1', 'State 1', '1234567890', 'P12345678', '2025-12-31', 'Country 1', '1990-01-01'),
+       ('customer2@example.com', 'Thomas Two', 'password2', '456', 'Street 2', 'City 2', 'State 2', '9876543210', 'P23456789', '2026-12-31', 'Country 2', '1991-01-01'),
+       ('customer3@example.com', 'Tim Three', 'password3', '789', 'Street 3', 'City 3', 'State 3', '1111111111', 'P34567890', '2027-12-31', 'Country 3', '1992-01-01'),
+       ('customer4@example.com', 'Freddie Four', 'password4', '101', 'Street 4', 'City 4', 'State 4', '2222222222', 'P45678901', '2028-12-31', 'Country 4', '1993-01-01');
 
-INSERT INTO booking_agent (email, password, booking_agent_id, airline_name) 
-VALUES ('agentA@example.com', 'agentApassword', 'A12345', 'China Eastern');
+-- Adding booking agents working for different airlines
+INSERT INTO booking_agent (email, password, airline_name) 
+VALUES ('agentA@example.com', 'agentApassword', 'Delta Air Lines'),
+       ('agentB@example.com', 'agentBpassword', 'American Airlines'),
+       ('agentC@example.com', 'agentCpassword', 'Delta Air Lines');
 
--- d
-INSERT INTO airplane (id, airline_name) 
-VALUES ('Plane1', 'China Eastern'), 
-       ('Plane2', 'China Eastern');
+-- Adding airplanes to the system
+INSERT INTO airplane (airline_name) 
+VALUES ('China Eastern'),
+       ('American Airlines'), 
+       ('Delta Air Lines');
 
--- e
+-- Adding airline staff with different permissions and airlines
+-- "Admin" permission staff can create flights, add airplanes/airports, grant permissions
+-- "Operator" permission staff can change flight status
+-- NULL permission staff can only view flights
 INSERT INTO airline_staff (username, password, first_name, last_name, date_of_birth, permission, airline_name) 
-VALUES ('staff1', 'password1', 'Staff', 'One', '1980-01-01', 'Full', 'China Eastern');
+VALUES ('adminStaff', 'adminPass', 'Alice', 'Admin', '1975-01-01', 'Admin', 'American Airlines'),
+       ('operatorStaff', 'operatorPass', 'Oscar', 'Operator', '1985-01-01', 'Operator', 'Delta Air Lines'),
+       ('normalStaff', 'normalPass', 'Nick', 'Normal', '1995-01-01', NULL, 'American Airlines');
 
--- f
-INSERT INTO flight (flight_num, airline_name, departure_time, arrival_time, price, status, airplane_id, arr_airport_name, dept_airport_name) 
-VALUES (1, 'China Eastern', '2023-11-01 10:00:00', '2023-11-01 14:00:00', 200.00, 'Upcoming', 'Plane1', 'JFK', 'PVG'),
-       (2, 'China Eastern', '2023-10-01 10:00:00', '2023-10-01 14:00:00', 250.00, 'InProgress', 'Plane2', 'PVG', 'JFK'),
-       (3, 'China Eastern', '2023-09-01 10:00:00', '2023-09-01 14:00:00', 300.00, 'Delayed', 'Plane1', 'JFK', 'PVG');
+-- Adding more flights covering various statuses and airlines
+INSERT INTO flight (airline_name, departure_time, arrival_time, price, status, airplane_id, arr_airport_name, dept_airport_name) 
+VALUES ('China Eastern', '2023-11-01 10:00:00', '2023-11-01 14:00:00', 200.00, 'Upcoming', 1, 'JFK', 'PVG'),
+       ('China Eastern', '2023-10-01 10:00:00', '2023-10-01 14:00:00', 250.00, 'InProgress', 1, 'PVG', 'JFK'),
+       ('China Eastern', '2023-09-01 10:00:00', '2023-09-01 14:00:00', 300.00, 'Delayed', 1, 'JFK', 'PVG'),
+       ('American Airlines', '2023-12-01 10:00:00', '2023-12-01 15:00:00', 350.00, 'Cancelled', 2, 'LAX', 'HND'),
+       ('Delta Air Lines', '2023-12-02 11:00:00', '2023-12-02 16:00:00', 400.00, 'Delayed', 3, 'LHR', 'LAX');
 
--- g
-INSERT INTO ticket (ticket_id, flight_num, airline_name, customer_email, booking_agent_email) 
-VALUES ('T1', 1, 'China Eastern', 'customer1@example.com', NULL), 
-       ('T2', 2, 'China Eastern', 'customer2@example.com', 'agentA@example.com');
+-- Adding tickets for new flights and customers
+INSERT INTO ticket (flight_num, airline_name, customer_email) 
+VALUES (1, 'China Eastern', 'customer3@example.com'),
+       (1, 'China Eastern', 'customer2@example.com'), 
+       (2, 'China Eastern', 'customer2@example.com'),
+       (3, 'China Eastern', 'customer1@example.com'),
+       (4, 'American Airlines', 'customer1@example.com'),
+       (5, 'Delta Air Lines', 'customer4@example.com'),
+       (5, 'Delta Air Lines', 'customer3@example.com');
