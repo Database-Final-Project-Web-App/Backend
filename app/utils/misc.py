da4e2c8f788737d370d67fdcfa2e24925a38f892
Xinyu Li <eigerli@outlook.com>
2023-12-01 20:46:50 +0800

    add LOGINTYPE constants

--- app/utils/misc.py
+++ app/utils/misc.py
@@ -1,2 +1,10 @@
 def TODO(text=""):
 	assert("TODO: {}".format(text))
+
+# define constant of logintype
+class LOGINTYPE:
+	CUSTOMER = 'customer'
+	BOOKING_AGENT = 'booking_agent'
+	AIRLINE_STAFF = 'airline_staff'
+
+COOKIE_MAX_AGE = 100000000
